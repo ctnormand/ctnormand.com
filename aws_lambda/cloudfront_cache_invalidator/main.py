e26aed30a1433e62cde9562ad67e28492f6028fb
Christian Normand <ctnormand1@gmail.com>
2023-06-16 11:08:52 -0600

    create lambda function to automatically invalidate cloudfront cache

---
+++ aws_lambda/cloudfront_cache_invalidator/main.py
@@ -0,0 +1,47 @@
+"""AWS Lambda function to automatically create cloudfront invalidations when S3
+objects are created or deleted.
+"""
+import logging
+import os
+import time
+
+import boto3
+
+CLOUDFRONT_DISTRIBUTION_ID = os.environ.get("CLOUDFRONT_DISTRIBUTION_ID")
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+cloudfront_client = boto3.client("cloudfront")
+
+
+def handler(event, context=None):
+    """Creates an invalidation when an S3 object is created or deleted."""
+    for record in event["Records"]:
+        bucket = record["s3"]["bucket"]["name"]
+        key = record["s3"]["object"]["key"]
+        paths = [f"/{key}"]
+
+        # Must invalidate additoinal paths if modified file is index.html
+        if os.path.basename(key) == "index.html":
+            paths.append(f"/{os.path.dirname(key)}")
+            if os.path.dirname(key) != "":
+                paths.append(f"/{os.path.dirname(key)}/")
+        try:
+            res = cloudfront_client.create_invalidation(
+                DistributionId=CLOUDFRONT_DISTRIBUTION_ID,
+                InvalidationBatch={
+                    "Paths": {"Quantity": len(paths), "Items": paths},
+                    "CallerReference": str(int(time.time() * 1000)),
+                },
+            )
+        except Exception as e:  # noqa: F841
+            log.exception(
+                f"Failed to create invalidation for {bucket}{key} on CloudFront "
+                f"distribution {CLOUDFRONT_DISTRIBUTION_ID}."
+            )
+        else:
+            log.info(
+                f"Created invalidation for {bucket}{key} on Cloudfront "
+                f"distribution {CLOUDFRONT_DISTRIBUTION_ID}. "
+                f"(Invalidation ID: {res['Invalidation']['Id']})."
+            )
